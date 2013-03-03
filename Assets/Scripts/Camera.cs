c0e63b18f7f362a5b1590bb107076b4993e4dec5
渡部 晴人 <dailioh@gmail.com>
2013-03-04 00:33:31 +0900

    Add Line

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -5,15 +5,17 @@ public class Camera : MonoBehaviour {
 	[SerializeField]
 	public float speed = 0.5f;
 	public float rotY = 0.0f;
+	
+	private float rotX = 0.0f;
 
 	// Use this for initialization
 	void Start () {
-	
+		rotX = this.transform.localEulerAngles.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		rotY += speed;
-		this.transform.rotation = Quaternion.Euler(90, rotY, 0);
+		this.transform.rotation = Quaternion.Euler(rotX, rotY, 0);
 	}
 }
