5779e084700cd837680c32120cdfb50eda780038
渡部 晴人 <dailioh@gmail.com>
2013-03-05 18:33:59 +0900

    Add ArrowKey

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -22,13 +22,13 @@ public class InputController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.A) || Input.GetKeyDown(KeyCode.A))
+		if (Input.GetKey(KeyCode.A) || Input.GetKeyDown(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
 		{
 			Debug.Log("A pushed");
 			theta += speed;
 			this.transform.position = new Vector3 (x + r * Mathf.Cos(theta), 0, z + r * Mathf.Sin(theta));
 		}
-		else if (Input.GetKey(KeyCode.D) || Input.GetKeyDown(KeyCode.D))
+		else if (Input.GetKey(KeyCode.D) || Input.GetKeyDown(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.RightArrow))
 		{
 			Debug.Log("D pushed");
 			theta -= speed;
